5ebbe3ab9a349e4dc39715d491ad3f85f5407849
8rz3zin <jak.brz482@gmail.com>
2024-05-05 14:07:19 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StoreManaging.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
